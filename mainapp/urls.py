7e4822dc3cfaecde404881c76aff17a02a25d0da
Andrés Escobar <andres.escobar.hdez@gmail.com>
2021-02-03 01:34:01 -0600

    PÁGINA DE LOG DE AUTENTICAICIÓN

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('inicio/', views.inicio_page, name='inicio'),
     path('login/', views.login_page, name='login'),
     path('logout/', views.logout_user, name='logout'),
+    path('login-log/', views.login_logs, name='login_logs'),
+    path('filter-login-log/', views.filter_login_logs, name='filter_login_logs'),
 ]
--- mainapp/views.py
+++ mainapp/views.py
@@ -1,7 +1,13 @@
-from django.shortcuts import redirect, render
+from django.shortcuts import redirect, render, HttpResponse
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
+from django.http import JsonResponse
+from django.core.serializers import serialize
+
+from .models import LoginLog
+
+import datetime
 
 # Create your views here.
 def index_page(request):
@@ -24,6 +30,7 @@ def login_page(request):
         user  = authenticate(request, username = username, password = password)
         if user is not None:
             login(request, user)
+            log = LoginLog.objects.create(user = user)
             return redirect('inicio')
         else:
             messages.warning(request, 'El usuario o contraseña son incorrectos.')
@@ -32,3 +39,17 @@ def login_page(request):
 def logout_user(request):
     logout(request)
     return redirect('login')
+
+@login_required(login_url='login')
+def login_logs(request):
+    return render(request, 'usuario/login-log.html')
+
+@login_required(login_url='login')
+def filter_login_logs(request):
+    rango = request.GET.get('filter_date')
+    fechas = rango.split(' - ')
+    fecha_inicio = datetime.datetime.strptime(fechas[0], "%Y-%m-%d %H:%M:%S")
+    fecha_fin = datetime.datetime.strptime(fechas[1], "%Y-%m-%d %H:%M:%S")
+    logs = LoginLog.objects.filter(created_at__range=(fecha_inicio, fecha_fin))
+    html = render(request, 'usuario/login-log-rows.html', {"logs": logs})
+    return HttpResponse(html)
